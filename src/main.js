5c7e78b7b7a2e9dbc45a3c47600c693712e0a665
xuxiaojian <xuxiaoxian@gmail.com>
2022-08-30 17:30:04 +0800

    add practise

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Button,Radio} from 'element-ui';
+import router from '../router'
+
+
+
 
 Vue.config.productionTip = false
+Vue.component(Button.name, Button);
+Vue.component(Radio.name, Radio);
+
+
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ views/Home.vue
@@ -0,0 +1,15 @@
+
+<template>
+    <div>
+        我是Home页面
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Home',
+        data(){
+            return{}
+        }
+    }
+</script>
